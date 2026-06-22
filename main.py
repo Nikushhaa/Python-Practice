7c96258ca1457461cbc2b1a467ebc133a373026f
Nikusha <nevermissacc@gmail.com>
2026-06-22 15:37:51 +0400

    Calculate hypothenuse of a right triangle using theorem

--- main.py
+++ main.py
@@ -169,17 +169,21 @@
 #print(f"The circumference is: {round(circumference,2)}cm")
 
 
-import math
+#import math
 
-radius = float(input("Enter the radius of a circle:"))
+#radius = float(input("Enter the radius of a circle:"))
 
-area = math.pi  * pow(radius,2)
+#area = math.pi  * pow(radius,2)
 
-print(f"The area of this circle is: {area}cm^2")
-
-
+#print(f"The area of this circle is: {round(area, 2)}cm^2")
 
 
+#import math 
+
+#a = float(input("Enter side A:"))
+#b = float(input("Enter side B:"))
+#c = math.sqrt(pow(a,2)+ pow(b,2))
+#print(f"Side C = {c}")
 
 
 
