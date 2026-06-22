0910f91b791c07768e5f51dd7ca06a79a383bf11
Nikusha <nevermissacc@gmail.com>
2026-06-22 16:05:43 +0400

    calculator of python

--- main.py
+++ main.py
@@ -231,8 +231,26 @@
  #   print("The user is offline")
 
 
+#Python calculator
+
+operator = input("Enter an operator (+ - * / ): ")
+num1 = float(input("Enter the first number: "))  # aq sachiroa float
+num2 = float(input("Enter the second number: "))
+#print(num1 + num2) That's the first printing 
 
 
+if operator == "+":
+ result = num1 + num2
+ print(result)
+elif operator == "-":
+  result = num1 - num2
+  print(result)
+elif operator == "*":
+  result = num1 * num2
+  print(result)
+elif operator == "/":
+  result = num1 / num2
+  print(result)
 
 
 
