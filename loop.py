08ab09498ea71724f5094b1b294e8ef167081c37
Nikusha <nevermissacc@gmail.com>
2026-06-29 16:37:53 +0400

    commited prime numbers project v2

--- loop.py
+++ loop.py
@@ -27,43 +27,44 @@
 #     elif  number % 5 == 0:
 #             print(f"Buzz")
     
-import math  
+# import math  
 
+# def prime(number):
+#     if number <= 1:
+#      #tu ricxvi aris 1 ze naklebi an toli arari prime
 
-def prime(number):
-   
-
-    if number <= 1:
-     #tu ricxvi aris 1 ze naklebi an toli arari prime
-
-        print(f"{number} is not prime")
+#         print(f"{number} is not prime")
        
+#         return False      
+#     for i in range(2, int(math.sqrt(number)) + 1):
+#        #tu ricxvs erti gamyofi mainc aqvs is uechveli iqneba kvadratul fesvze patara an naklebi
+#         if number % i == 0:
+#         #tu ricxvs nashti 0 aqvs eseigi aqvs gamyofi da ar aris martivi
+#             print(f"{number} is not prime")
+        
+#             return False
+#             #vasrulebt funqcias
+#     print(f"{number} is prime")
+# #tu ricxvshi vercerti gamyofi ver moipoveba
+#     return True
+#     #tu ricxvi martivia
+# number = int(input("Number: "))
+# prime(number)
+import math
 
+def unprime(number):
+    if number <= 1:
+        print(f"{number} is not prime")
         return False
-      
-
 
     for i in range(2, int(math.sqrt(number)) + 1):
-       #tu ricxvs erti gamyofi mainc aqvs is uechveli iqneba kvadratul fesvze patara an naklebi
-
         if number % i == 0:
-        #tu ricxvs nashti 0 aqvs eseigi aqvs gamyofi da ar aris martivi
-
             print(f"{number} is not prime")
-            
-
-
             return False
-            #vasrulebt funqcias
-
 
     print(f"{number} is prime")
-#tu ricxvshi vercerti gamyofi ver moipoveba
-
     return True
-    #tu ricxvi martivia
 
 
-
-number = int(input("Number: "))
-prime(number)
+number = int(input("Random number = "))
+unprime(number)
