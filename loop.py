7d30737b028a8bbfd73605f902113b5b9ea4c295
Nikusha <nevermissacc@gmail.com>
2026-07-13 15:45:47 +0400

    logical operatorr

--- loop.py
+++ loop.py
@@ -220,11 +220,24 @@
 #                                  or = at least one condition must be true
 #                                  and = both conditions must be true
 #                                  not = inverts the condition (not false, not True)
-temp = 25
-is_raining = False
+# temp = 25
+# is_raining = False
 
-if temp > 35 or temp< 0 or is_raining:
-    print("The outdoor event is canceled")
-else:
-    print("The outdoor event is still scheduled")
-                
+# if temp > 35 or temp< 0 or is_raining:
+#     print("The outdoor event is canceled")
+# else:
+#     print("The outdoor event is still scheduled")
+
+
+
+temp = -5
+is_sunny = True
+if temp >= 28 and is_sunny:
+    print("It is hot outside!")
+    print("It is sunny outside!")
+elif temp <= 0 and is_sunny:
+    print("It is freezing outside!")
+    print("It is sunny outside!")
+elif temp < 28 and temp > 0 and is_sunny:
+    print("It is warm outside!")
+    print("It is sunny outside!")              
