35d6784120e0e0963f8239e34f55d00a88571955
Nikusha <nevermissacc@gmail.com>
2026-07-17 16:38:04 +0400

    commited format specifiers

--- loop.py
+++ loop.py
@@ -322,12 +322,12 @@
 # :, = use a comma as a thousands separator
 
 
-price1 = 3.14159
-price2 = -987.65
-price3 =  12.34
-print(f"price 1 is ${price1:.2f}")
-print(f"price 2 is ${price2:.2f}")
-print(f"price 3 is ${price3:.2f}")
+# price1 = 3.14159
+# price2 = -987.65
+# price3 =  12.34
+# print(f"price 1 is ${price1:.2f}")
+# print(f"price 2 is ${price2:.2f}")
+# print(f"price 3 is ${price3:.2f}")
 
 
 
